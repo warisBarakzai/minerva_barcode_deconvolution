6ccf38cf418030109da1bc2e1dee0c00633ab794
Zihao Wu <zw1269@nyu.edu>
2019-05-06 08:49:40 -0400

    display

--- minerva/deconvolution/suffix_tree.py
+++ minerva/deconvolution/suffix_tree.py
@@ -4,7 +4,7 @@ from itertools import combinations
 import numpy as np
 import sys
 
-class End:
+class End:  #define global end
 	def __init__(self, val = -1):
 		self.val = val
 
@@ -12,19 +12,30 @@ class End:
 		return self.val
 	def __repr__(self):
 		return str(self.val)
-	def __gt__(self, other):
+	def __gt__(self, other):  #over load greater than operator 
 		return self.val > other.val
+	def __str__(self):
+		return str(self.val)
+	def __index__(self):
+		return self.val
+	def __int__(self):
+		return self.val
+	def __add__(self,num):
+		return self.val + num
 
 
 class Node:
 	def __init__(self):
 		#			  a(Edge, Node)  t 				c 			g 	 
 		self.atcg = [[None, None], [None, None], [None, None], [None, None]]
+		# only four characters 
 		self.suffixLink = None
+		self.data = ""
 
 class SuffixTree:
 	def __init__(self, readList, End):
 		self.root = Node()
+		self.root.data = readList
 		self.end = End
 		self.build_tree(readList)
 
@@ -38,17 +49,46 @@ class SuffixTree:
 		elif character == 'G':
 			return 3
 
+	def walk_dfs(self,current,visited=None,char=""):
+		if visited is None:
+			visited = set()
+		visited.add(current)
+		counter = 0
+
+		while(  counter < len(current.atcg) and  current.atcg[counter][1] == None):
+			if(current.atcg[counter][0] != None):
+				start,end = current.atcg[counter][0][0],current.atcg[counter][0][1]
+				print(start,end)
+				print(current.data[start:end+1])
+				#while the next field is none, print all the suffixes before that
+				# print(current.atcg[counter])
+			counter += 1
+
+		for i in range( len(current.atcg)):
+			if(current.atcg[i][1] != None and current.atcg[i][1] not in visited):
+				start,end = current.atcg[i][0][0], current.atcg[i][0][1]
+				char = current.data[start:end+1]
+				print(char,current.data[start:end+1],end="")
+				#store the current char (this part is incomplete it would be better to store a chararray)
+				# print(current.atcg[i][0],current.atcg[i][1])
+				# print(current.data[start:end+1])
+				self.walk_dfs(current.atcg[i][1],visited,char)
+		return visited
+
+
 	def build_tree(self, read):
 		aNode = self.root
-		aEdge = None 
-		aPos = 0
+		aEdge = None  #active edge
+		aPos = 0 # active position
 		rem = 0
 		for i in range(len(read)):
 			print("rem:", rem)
 			# print("Node:", aNode,"//Edge:", aEdge,"//Pos:", aPos)
 			checking_string = read[:i+1]
-			print(checking_string)
+			# print("checking_string",checking_string)
 			edge_label = self.get_index(read[i])
+			# current letter
+			#get the edge index 0 =A ETC.
 			rem += 1
 			self.end.val += 1
 			first = False
@@ -58,22 +98,37 @@ class SuffixTree:
 			while rem > 0:
 				if aPos == 0:
 					if (aEdge == None or aEdge[0] == None) and aNode.atcg[edge_label][0] == None and aNode.atcg[edge_label][1] == None:
-						aNode.atcg[edge_label] = [[i, self.end], None]
+						# active edge = none    and active node's current letter has not been initialized
+						# print("aedge[0] = ",aEdge[0])
+						print("aedge= " ,aEdge)
+						aNode.atcg[edge_label] = [[i, self.end], None] 
+						#initialize current letter new node
 						rem -= 1
 					elif (aEdge == None or aEdge[0] == None) and aNode.atcg[edge_label][0] != None:
+						# if node has been initialized  
 						aEdge = aNode.atcg[edge_label]
+						#set active edge
 						aPos = 1
 						if  aEdge[0][1] != self.end and aEdge[0][0] + aPos > aEdge[0][1]:
+							#whats the point of checking [0][1] != end 
+							# edge + apos > edge[0][1] means not valid
+							print("aedge= " ,aEdge)
 							aNode = aEdge[1]
+
 							aEdge = None
 							aPos = 0
+							#reset
 						break
 					elif aEdge[0] != None:
+						#has been initialized
+						print("aedge= " ,aEdge)
 						aPos = 1
 						if  aEdge[0][1] != self.end and aEdge[0][0] + aPos > aEdge[0][1]:
+							#if this is not the last node and this is not the edge that we are looking for
 							aNode = aEdge[1]
 							aEdge = None
 							aPos = 0
+							#reset
 						break
 				else:
 					if read[aEdge[0][0]+aPos] == read[i]:
@@ -82,6 +137,7 @@ class SuffixTree:
 							aNode = aEdge[1]
 							aEdge = None
 							aPos = 0
+						#hit rule stopper increment active position do nothing
 						break  
 					# Commented code - This is never going to happen because if the second is end Edge+position < end
 					# elif aEdge[0][1] == self.end and End(aEdge[0][0] + aPos) > aEdge[0][1]:
@@ -93,8 +149,9 @@ class SuffixTree:
 					# 		break
 					else:
 						#  HAVE NO CODE FOR DEALING WITH SUFFIX LINKS AND NOT HAVING ROOT AS SUFFIXLINKS
+						print("aedge= " ,aEdge)
 						inserting_prefix = read[i-rem+1:i+1]
-						print("inserting prefix:", inserting_prefix)
+						# print("inserting prefix:", inserting_prefix)
 						new_edge_label = self.get_index(inserting_prefix[-1])
 						# This shouldn't be in an if loop...
 						# if read[aEdge[0][0]+aPos] != inserting_prefix[-1]:
@@ -102,9 +159,12 @@ class SuffixTree:
 						prev = aEdge[1]
 						print("prev:", prev)
 						aEdge[1] = Node()
+						aEdge[1].data = read
+						#creating a new node at active position
 						aEdge[1].atcg[new_edge_label] = [[i, self.end], None]
 						branch_edge_label = self.get_index(read[aEdge[0][0]+aPos])
-						aEdge[1].atcg[branch_edge_label] = [[aEdge[0][0]+aPos, aEdge[0][1]], prev] #changed second value from None to aEdge[1]
+						aEdge[1].atcg[branch_edge_label] = [[aEdge[0][0]+aPos, aEdge[0][1]], prev] 
+						#changed second value from None to aEdge[1]
 						aPos -= 1
 						aEdge[0] = [aEdge[0][0], aEdge[0][0]+aPos]
 						print(aEdge)
@@ -134,12 +194,26 @@ class SuffixTree:
 							aPos = 0
 						print("After:", rem)
 
-		print("final rem:", rem)
-			
+		# print("final rem:", rem)
+
 			
 
 
 end = End()
-tree = SuffixTree("CAACGCGGCGGCGAGGACGCCGCCCGCGGCCGGCGCCACCCACGCCGGCAGGCGGGTGAGCAGCAGGGCGAGCGTCAGCAGGATGGGGGTGAGCGCGAGCAGGACCTGCACCGGCACCCCCTGATCG", end)
-print(tree.root.atcg)
+tree = SuffixTree("ATCGTTCTGC", end)
+temp = tree.root.atcg
+print("display root",tree.root.atcg)
+print("display second node",tree.root.atcg[1][1].atcg)
+print("display second node",tree.root.atcg[2][1].atcg)
+print("display third node",tree.root.atcg[1][1].atcg[2][1].atcg)
+result = tree.walk_dfs(tree.root)
+counter = 0
+length = len(tree.root.data)
+print(len(tree.root.data))
+
+
+
+
+
+
 
